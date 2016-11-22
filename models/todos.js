ca99cdeee801461e193921f16ae225f18bab0833
Nicolas AUGER <nicolas.auger@ynov.com>
2016-11-22 17:12:09 +0100

    Model for Todos

---
+++ models/todos.js
@@ -0,0 +1,99 @@
+const mongoose = require('mongoose')
+const moment = require('moment')
+moment.locale('fr')
+
+var todoSchema = mongoose.Schema({
+    author : {type: String, required: true},
+    task : {type: String, required: true},
+    recipient : {type: String, required: true},
+    is_completed: {type: Boolean, default: false},
+    //team : {type: String},
+    createdAt: {type: Date, default: Date.now},
+    modifiedAt: {type: Date, default: null},
+    completedAt: {type: Date, default: null}
+})
+
+var todoModel = mongoose.model('todos', todoSchema)
+
+module.exports = {
+
+    get: (id, callback) => {
+        //console.log('id :',id);
+        todoModel.find({"_id": id}, function(err, todo) {
+            //console.log(todo);
+            if(!err){
+                callback(todo[0])
+            }else{
+                throw err
+            }
+        })
+    },
+
+
+    getAll: (callback) => {
+        todoModel.find({}, function(err, todos) {
+            if(!err){
+                callback(todos)
+            }else{
+                throw err
+            }
+        })
+    },
+
+
+    insert: (body, callback) => {
+        var newTodo = new todoModel()
+
+        newTodo.author = body.author
+		newTodo.task = body.task
+        newTodo.recipient = body.recipient
+        //newTodo.team = body.team
+        //newTodo.createdAt = moment().calendar()
+        newTodo.save(function(err, data) {
+            if (err){console.log('Error : ', err)}
+            else{callback(data)}
+        })
+    },
+
+
+    update: (id, body, callback) => {
+  		todoModel.findById(id, function (err, todo) {
+  			if (err) throw(err)
+            //console.log(body)
+            todo.author = body.author
+    		todo.task = body.task
+            todo.recipient = body.recipient
+            // todo.team = body.team
+            todo.modifiedAt = Date.now()
+
+  			todo.save(function (err) {
+  				if (err) throw(err)
+  				callback()
+  			})
+            //}
+  		})
+  	},
+
+
+    remove: (id ,callback) => {
+		todoModel.remove({ "_id": id }, function(err) {
+			if (!err) {callback('success')}
+			else {message.type = 'error'}
+		});
+	},
+
+
+    complete: (id, callback) => {
+        todoModel.findOne({"_id": id}, function(err, todo) { // Faire findOne plutôt que Find et return todo[0].
+            //console.log(todo);
+            todo.is_completed = true
+            todo.completedAt = Date.now()
+            todo.save(function (err) {
+  				if (err) throw(err)
+  				callback(todo)
+  			})
+        })
+    },
+
+
+}
